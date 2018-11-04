952af6f9808051168e8a80e4c9ca68a2625815f6
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-04 22:10:08 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,9 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
   loop do
-    
+    number_of_times += 1 
+    puts "Welcome to Flatiron School's Web Development Course!"
+    if number_of_times >= 7
     
   
 end
